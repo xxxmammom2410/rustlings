60dd6005979288972fc517ba010da4e010c00364
xxxmammom2410 <akira2410.luminous@gmail.com>
2023-01-01 02:09:16 +0000

    variable5,6 const,shadowing

--- exercises/variables/variables5.rs
+++ exercises/variables/variables5.rs
@@ -1,11 +1,12 @@
 // variables5.rs
 // Execute `rustlings hint variables5` or use the `hint` watch subcommand for a hint.
 
-
-
 fn main() {
     let mut number = "T-H-R-E-E"; // don't change this line
     println!("Spell a Number : {}", number);
-    number = 3 as i32; // don't rename this variable
-    println!("Number plus two is : {}", number + 2);
+    {
+        let number;
+        number = 3; // don't rename this variable
+        println!("Number plus two is : {}", number + 2);
+    }
 }
