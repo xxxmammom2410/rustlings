ceec1cc8d7af0d636bff00813c927f282bcf9cfc
xxxmammom2410 <akira2410.luminous@gmail.com>
2023-01-14 08:39:13 +0000

    string3 done

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -1,21 +1,28 @@
 // strings3.rs
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    input.trim_matches(' ').to_string()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+
+    // mut String　の場合
+    // input.to_string().push_str(" world!")
+    input.to_owned() + " world!"
+    // format!("{}{}",input," world!")
+    // [input,"world!"].connect(" ")
+    // [input," world!"].concat()
+    
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    // ???
+    input.replace("cars","balloons")
 }
 
 #[cfg(test)]
