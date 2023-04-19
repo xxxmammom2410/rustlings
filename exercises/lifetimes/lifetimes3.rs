453e6956655266b82a78f21bf97d585e51fd8294
xxxmammom2410 <akira2410.luminous@gmail.com>
2023-04-19 15:23:18 +0000

    lifetimes3 done

--- exercises/lifetimes/lifetimes3.rs
+++ exercises/lifetimes/lifetimes3.rs
@@ -4,16 +4,15 @@
 //
 // Execute `rustlings hint lifetimes3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
-struct Book {
-    author: &str,
-    title: &str,
+struct Book <'a> {
+    author: &'a str,
+    title: &'a str,
 }
 
 fn main() {
     let name = String::from("Jill Smith");
     let title = String::from("Fish Flying");
+    // 構造体が文字列スライスへの参照を持つ
     let book = Book { author: &name, title: &title };
 
     println!("{} by {}", book.title, book.author);
