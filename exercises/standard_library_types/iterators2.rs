a38c16dc801c00464a0f5b87b0858cc0fb012dce
xxxmammom2410 <akira2410.luminous@gmail.com>
2023-04-21 08:23:36 +0000

    iterators2 done

--- exercises/standard_library_types/iterators2.rs
+++ exercises/standard_library_types/iterators2.rs
@@ -9,10 +9,13 @@
 // Complete the `capitalize_first` function.
 // "hello" -> "Hello"
 pub fn capitalize_first(input: &str) -> String {
+    // char型の配列にする
     let mut c = input.chars();
     match c.next() {
         None => String::new(),
-        Some(first) => ???,
+        // c.next()の値がNoneでないとき,
+        Some(first) => first.to_string().to_uppercase()+c.as_str()
+        // first.to_string().to_uppercase(),
     }
 }
 
@@ -21,15 +24,34 @@ pub fn capitalize_first(input: &str) -> String {
 // Return a vector of strings.
 // ["hello", "world"] -> ["Hello", "World"]
 pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
-    vec![]
-}
+    // 文字列スライスの配列を参照で受け取る
+    // 配列からイテレータを作成
+    // capitalize_firstは参照を受け取るのでiter()メソッドを使用する
+    let mut iterable_words = words.iter();
+    let mut _capitalized_words: Vec<String> = vec![];
+
+    // イテレータを使って各要素にcapitalize_first()を適用
+    for word in iterable_words{
+        _capitalized_words.push(capitalize_first(word).to_string())
+    }
+
+    _capitalized_words
+
+}   
 
 // Step 3.
 // Apply the `capitalize_first` function again to a slice of string slices.
 // Return a single string.
 // ["hello", " ", "world"] -> "Hello World"
-pub fn capitalize_words_string(words: &[&str]) -> String {
-    String::new()
+pub fn capitalize_words_string(_words: &[&str]) -> String {
+    let mut _capitalized_words_vecotr: Vec<String> = vec![];
+    // イテレータを使って各要素にcapitalize_first()を適用
+    for word in _words.iter(){
+        _capitalized_words_vecotr.push(capitalize_first(word).to_string())
+    }
+
+    // 最後に結合する
+    _capitalized_words_vecotr.concat()
 }
 
 #[cfg(test)]
